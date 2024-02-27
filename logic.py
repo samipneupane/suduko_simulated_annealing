525ea98b11308b2056c4d25f03e1c831c6e8a5ea
samipneupane <samipneupane335@gmail.com>
2024-02-27 20:21:59 +0545

    ui created for annealing process

--- logic.py
+++ logic.py
@@ -272,3 +272,103 @@ def get_startend_time():
     global start_time
     global end_time
     return start_time, end_time
+
+
+
+def draw_grid(canvas):
+    # Draw horizontal lines
+    for i in range(10):
+        width = 2 if i % 3 == 0 else 1
+        canvas.create_line(50, 50 + i * 50, 500, 50 + i * 50, width=width)
+
+    # Draw vertical lines
+    for i in range(10):
+        width = 2 if i % 3 == 0 else 1
+        canvas.create_line(50 + i * 50, 50, 50 + i * 50, 500, width=width)
+
+
+
+def draw_numbers(canvas, puzzle):
+    for i in range(9):
+        for j in range(9):
+            if puzzle[i][j] != 0:
+                global fixed_sudoku
+                if fixed_sudoku[i][j] == 1:
+                    canvas.create_text(75 + j * 50, 75 + i * 50, text=str(puzzle[i][j]), font=("Arial", 18, "bold"), fill="red")
+                else:
+                    canvas.create_text(75 + j * 50, 75 + i * 50, text=str(puzzle[i][j]), font=("Arial", 16))
+            else:
+                canvas.create_text(75 + j * 50, 75 + i * 50, text=str(0), font=("Arial", 16))
+
+
+
+def draw_score(canvas, score):
+    canvas.delete("all")
+    canvas.create_text(100, 30, text="Error Score: " + str(score), font=("Arial", 16))
+
+
+
+def draw_time(canvas, time):
+    canvas.delete("all")
+    start_time, end_time = time
+    multiline_text = f"Start Time: {start_time}\nEnd Time: {end_time}"
+    canvas.create_text(200, 50, text=multiline_text, font=("Arial", 12))
+
+
+
+def draw_value_bar(canvas, value):
+    canvas.delete("all")
+    canvas_width = 40
+    canvas_height = 550
+    bar_height = value * canvas_height / 5.0
+    canvas.create_rectangle(0, canvas_height - bar_height, canvas_width, canvas_height, fill="blue")
+    canvas.create_text(canvas_width + 15, 10, text="5.00", anchor="nw", font=("Arial", 10, "bold"))
+    canvas.create_text(canvas_width + 15, 540, text=f"0.00", anchor="nw", font=("Arial", 10, "bold"))
+    canvas.create_text(canvas_width - 35, 565, text="Tempr", anchor="nw", font=("Arial", 12, "bold"))
+    canvas.create_text(canvas_width + 15, 250, text=f"{value:.2f}", anchor="nw", font=("Arial", 15, "bold"))
+
+
+
+def new_puzzle(root, canvas_box, canvas_score, canvas_time, canvas_temp):
+    canvas_box.delete("all")
+    puzzle = get_current_sudoku()
+    draw_numbers(canvas_box, puzzle)
+    draw_grid(canvas_box)
+    
+    score = get_current_score()
+    draw_score(canvas_score, score)
+
+    sigma = get_current_tempr()
+    draw_value_bar(canvas_temp, sigma)
+
+    if(is_solution_found()):
+        time = get_startend_time()
+        draw_time(canvas_time, time)
+
+    root.after(10, new_puzzle, root, canvas_box, canvas_score, canvas_time, canvas_temp)
+
+
+
+def start_ui():
+    root = tk.Tk()
+    root.title("Sudoku Solver - Simulated Annealing")
+    root.geometry("1280x720")
+
+    canvas_box = tk.Canvas(root, width=500, height=500)
+    canvas_box.pack()
+    canvas_box.place(x=750, y=0)
+
+    canvas_score = tk.Canvas(root, width=200, height=60, bg="lightgray")
+    canvas_score.pack()
+    canvas_score.place(x=950, y=520)
+
+    canvas_time = tk.Canvas(root, width=400, height=100, bg="lightblue")
+    canvas_time.pack()
+    canvas_time.place(x=850, y=600)
+
+    canvas_temp = tk.Canvas(root, width=100, height=600, bg="lightgreen")
+    canvas_temp.pack()
+    canvas_temp.place(x=0, y=50)
+
+    root.after(10, new_puzzle, root, canvas_box, canvas_score, canvas_time, canvas_temp)
+    root.mainloop()
